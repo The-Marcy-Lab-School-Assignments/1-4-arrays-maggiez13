6b0dd844af912918019b216d479a291838e1c7dc
Mei Zhu <maggiez0613@gmail.com>
2024-04-09 17:44:08 -0400

    q12 done

--- src/debug.js
+++ src/debug.js
@@ -6,11 +6,17 @@ const clearArr = (arr) => {
   return arr; 
 };
 
-console.log(clearArr([1, 2, 3, 4, 5]))
+// console.log(clearArr([1, 2, 3, 4, 5]))
 
 const getFirstItem = (array) => {
-  return array.shift();
-};
+  // return array[0];
+  const copy = [...array];
+  return copy.shift();
+}; 
+
+console.log(getFirstItem([1, 2, 3, 4, 5]));
+console.log(getFirstItem(['a', 'b', 'c']));
+
 
 module.exports = {
   clearArr,
