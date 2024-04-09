479b6c9b8f2f3402084bd37c9f875638cec2a2f4
Mei Zhu <maggiez0613@gmail.com>
2024-04-09 17:25:52 -0400

    q11 done

--- src/debug.js
+++ src/debug.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-param-reassign */
 const clearArr = (arr) => {
-  arr = [];
+  while (arr.length > 0) {
+    arr.pop();
+  }
+  return arr; 
 };
+
+console.log(clearArr([1, 2, 3, 4, 5]))
 
 const getFirstItem = (array) => {
   return array.shift();
