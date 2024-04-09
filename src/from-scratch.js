5cbe6e5cc8a715e6dc415087ea860df83584dce6
Mei Zhu <maggiez0613@gmail.com>
2024-04-09 13:11:31 -0400

    q3 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,11 +1,27 @@
-const addToFrontOrBack = () => {
+const addToFrontOrBack = (arr, value, isFront) => {
+  if (isFront === true) {
+    console.log(arr.unshift(value));
+  } else {
+    console.log(arr.push(value));
+  }
 };
 
-const reverseString = () => {
+const reverseString = (string) => {
+  return string.split("").reverse().join("");
 };
 
-const newArrayFullOf = () => {
+// console.log(reverseString("hello"));
+
+const newArrayFullOf = (value, numOfValue) => {
+  let arr = [];
+  for (let i = 0; i < numOfValue; i++) {
+    console.log(arr.push(value));
+  }
+  return arr;
+  // console.log(array.fill(value));
 };
+
+console.log(newArrayFullOf(5, 3));
 
 const insertIntoMiddle = () => {
 };
